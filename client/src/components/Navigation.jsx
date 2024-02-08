4fccf015c710f7cb20387c8c80c502da7bf1a18c
JOHNINFA <johningonzalez2021@gmail.com>
2024-02-08 14:01:06 -0500

    cambios

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -5,11 +5,11 @@ const Navigation = () => {
   return (
     <div className="flex justify-between py-3">
    <Link to="/tasks">  
-   <h1 className=" font bold text-3xl mb-4"> Tasks App</h1> </Link>
+   <h1 className=" font bold text-3xl mb-4"> Task App</h1> </Link>
     
 
     <button className="bg-indigo-500 px3 py-2 rounded-lg">
-    <Link to= "/tasks-create" > create tasks </Link>
+    <Link to= "/tasks-create" > create task </Link>
     </button>
 
     </div>
